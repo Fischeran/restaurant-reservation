06baa90ab4f34f95d3c0acc9e74ccaf3c2a0203f
Fischer <alnfischer@gmail.com>
2022-12-07 08:23:45 -0500

    working on front end for last issue

--- front-end/src/ReservationList/ReservationList.js
+++ front-end/src/ReservationList/ReservationList.js
@@ -5,12 +5,15 @@ import { cancelReservation } from "../utils/api";
 
 
 function ReservationList({ reservations }) {
+const history = useHistory();
 
-  async function cancelReservation(event) {
-      const history = useHistory();
+
+  async function cancelReservation(res) {
+      
       const abortController = new AbortController()
-      if (window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
-        await cancelReservation(event.name, abortController.signal)
+      
+     if (window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
+        await cancelReservation(res, abortController.signal)
         history.go(0)
       } 
   }
@@ -26,7 +29,7 @@ function ReservationList({ reservations }) {
                       <p className="card-text">time: {res.reservation_time}</p>
                       <p className="car-test" data-reservation-id-status={res.reservation_id}>{res.status}</p>
                       {res.status === "booked" &&  <a href={`/reservations/${res.reservation_id}/seat`}><button>seat</button></a>}
-                      <button name={res.reservation_id} data-reservation-id-cancel={res.reservation_id} onClick={(event) => cancelReservation(event)}>Cancel</button>
+                      <button name={res.reservation_id} data-reservation-id-cancel={res.reservation_id} onClick={() => cancelReservation(res.reservation_id)}>Cancel</button>
                       <Link to={`/reservations/${res.reservation_id}/edit`}><button>Edit</button></Link>
                     </div>
         
